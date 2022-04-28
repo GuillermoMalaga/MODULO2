fe31b9117f61e3707aca8bdb3ecb5ee78a46a310
Guillermo <gmg.proyectos.2013@gmail.com>
2022-04-28 00:44:30 -0400

    ejercicio asincrono 02

---
+++ M02C01/js/M02C02_Condicionales.js
@@ -0,0 +1,4 @@
+var nombreGato="misifuz";
+var numTelefono=123123;
+var mayorEdad=true;
+var edad=prompt("Ingrese su edad")
---
+++ M02C01/js/M02C02_Ejercicios.js
@@ -0,0 +1,8 @@
+let x=10;
+let y="a";
+console.log("El valor de y===b es iogual a :");
+console.log(y==='b');
+console.log("El valor de x>=10 es igual a :");
+console.log(x>=10);
+console.log("El valor de la operacion total y==='b' || x>=10 es igual a:");
+console.log((y==='b' || x>=10));
